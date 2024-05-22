a8824ac096f05fe16c6191b3db36fde3c4ee638f
NI15RAJ <nirajmaheshwaribtechcse22-26@liet.in>
2024-05-22 05:53:02 +0000

    datatypes

---
+++ .devcontainer/test.js
@@ -0,0 +1,38 @@
+/*const accountId=144553
+let accountEmail="nirajmaheshwari@gmail.com"
+var accountpassword="12345"
+accountCity="Noida"
+let accountstate
+// accountId = 2
+
+
+accountEmail="nm@nm.com"
+accountpassword="12345678"
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountCity,accountstate]);
+
+// prefer not to use var
+// due to  issue in block space and functional space*/
+
+"use strict";
+
+//alert(3+3) we are using nodejs not browser
+
+console.log(3+3);
+
+console.log("Niraj");
+
+let name="NIraj"
+let age=21
+let isloggedin = false
+
+//primitive data types
+//number->2^53
+//bigint
+//string->""
+//numm->standalone value
+//undefined->
+//symbol->uniqueness
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
