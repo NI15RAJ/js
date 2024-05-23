1fb2274b4f3170f33d7226f3a74e382050a6ebcf
NI15RAJ <nirajmaheshwaribtechcse22-26@liet.in>
2024-05-23 06:03:45 +0000

    datatype summary

---
+++ .devcontainer/datatypesummary.js
@@ -0,0 +1,44 @@
+//  primitive datatypes
+//  7 types
+//  string
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+
+const score=100
+const scorevalue=100.3
+
+const isloggedin = false
+const outsidetemp=null
+let useremail
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id== anotherid);
+
+
+const bigNumber = 4873298342343324342n
+
+
+
+
+// reference (non primitive)
+// array
+// object
+// functions
+
+
+const heros=["shaktimaan","naagraj","doga"];
+let myObj={
+    name:"niraj",
+    age:21,
+}
+const myFunction=function(){
+    console.log("hello world");
+}
+console.log(typeof bigNumber);
